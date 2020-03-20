b9ebf0a017087b734b9b77031850b2ac3405e902
taybluetooth <callumtaylorbusiness@gmail.com>
2020-03-20 13:14:02 +0000

    Implemented precipitation and rain chance

--- backend/web-scrape.py
+++ backend/web-scrape.py
@@ -5,13 +5,26 @@ import re
 
 ## GLOBAL VARIABLE DECLARATION ##
 
-response = ""
-content = ""
-table = ""
+basic_response = ""
+basic_content = ""
+basic_table = ""
+detailed_response = ""
+detailed_content = ""
+detailed_table = ""
 regex = re.compile('([^\s\w]|_)+')
 
-def get_response(location):
+def get_basic_response(location):
     url = "https://www.forecast.co.uk/"+location+".html"
+    try:
+        response = requests.get(url, timeout = 5)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print("That location does not exist!")
+        sys.exit(1)
+    return response
+
+def get_detailed_response(location):
+    url = "https://www.forecast.co.uk/"+location+".html?v=detailed"
     try:
         response = requests.get(url, timeout = 5)
         response.raise_for_status()
@@ -70,33 +83,48 @@ def get_sun(table, type):
 def get_wind(table):
     row = table.findAll('tr')[4]
     wind = row.select("td")[0].text
-    wind = wind.strip('\n')
+    wind = wind.strip('\n').replace(' ', '')
     return wind
+
+def get_precipitation(table):
+    row = table.findAll('tr')[6]
+    precipitation = row.select("td")[0].text
+    precipitation = precipitation.strip('\n').replace(' ', '')
+    return precipitation
+
+def get_rain(table):
+    row = table.findAll('tr')[0]
+    rain = row.find("td", attrs = {'class' : 'rain'}).text.split('\n')[1].replace(' ', '')
+    return rain
 
 
 location = input("Please Enter Your Location: ").replace(' ', '-')
-response = get_response(location)
-content = get_content(response)
-table = get_data(content)
-condition = get_condition_array(table)
+response = get_basic_response(location)
+basic_content = get_content(response)
+basic_table = get_data(basic_content)
+condition = get_condition_array(basic_table)
+
+detailed = get_detailed_response(location)
+detailed_content = get_content(detailed)
+detailed_table = get_data(detailed_content)
 
 class Weather:
 
     def __init__(self):
         self.location = location.title()
-        self.max_temp = get_temp(table, "max")
-        self.min_temp = get_temp(table, "min")
+        self.max_temp = get_temp(basic_table, "max")
+        self.min_temp = get_temp(basic_table, "min")
         self.morning = get_morning(condition)
         self.afternoon = get_afternoon(condition)
         self.evening = get_evening(condition)
-        self.sunrise = get_sun(table, "sunrise")
-        self.sunset = get_sun(table, "sunset")
-        self.wind = get_wind(table)
+        self.sunrise = get_sun(basic_table, "sunrise")
+        self.sunset = get_sun(basic_table, "sunset")
+        self.wind = get_wind(basic_table)
+        self.precipitation = get_precipitation(basic_table)
+        self.rain_chance = get_rain(detailed_table)
         """
-        self.rain_chance = get_rain()
         self.humidity = get_humidity()
-        self.precipitation() = get_precipitation()
-        self.pressure() = get_pressure()
+        self.pressure = get_pressure()
         self.visibility = get_visibility()
         self.air_quality_index = get_air_index()
         self.air_quality = get_air_quality()
@@ -121,5 +149,8 @@ def main():
     print("~ Sunrise Time: " + weather.sunrise)
     print("~ Sunset Time: " + weather.sunset)
     print("~ Average Wind Speed: " + weather.wind)
+    print("~ Precipitation: " + weather.precipitation)
+    print("~ Chance of Rain: " + weather.rain_chance)
+
 
 main()
